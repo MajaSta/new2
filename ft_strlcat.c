2ef1f54d539ce7cd633f0cc18175b7477cf7f11b
MajaSta <142170733+MajaSta@users.noreply.github.com>
2024-07-02 16:39:43 +0200

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,35 +1,25 @@
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	dst_len;
-	size_t	src_len;
-	size_t	i;
-	size_t	j;
-
-	dst_len = 0;
-	src_len = ft_strlen(src);
-	i = 0;
-	while (dst[dst_len] && dst_len < size)
-		{
-			dst_len++;
-		}
-	if (dst_len == size)
-	{
-		return (size + src_len);
-	}
-	j = dst_len;
-	while (src[i] && (j + 1) < size)
-	{
-		dst[j] = src[i];
-		j++;
-		i++;
-	}
-	if (j < size)
-	{
-		dst[j] = '\0';
-	}
-	return (dst_len + src_len);
+    size_t dst_len = ft_strlen(dst);
+    size_t src_len = ft_strlen(src);
+    size_t total_len = dst_len + src_len;
+    size_t i = 0;
+    
+    if (size <= dst_len)
+    {
+        return total_len; // Jeśli size jest mniejsze lub równe długości dst, zwróć sumaryczną długość
+    }
+	
+    while (src[i] != '\0' && dst_len + 1 < size)
+    {
+        dst[dst_len + i] = src[i];
+        i++;
+    }
+    dst[dst_len + i] = '\0';
+    
+    return total_len;
 }
 /*
 #include <string.h>
