60b48dd8a4029079f28fb50e337fa6989c0cd428
MajaSta <142170733+MajaSta@users.noreply.github.com>
2024-07-02 16:55:28 +0200

    Update ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -14,13 +14,16 @@ char *ft_strrchr(const char *s, int c)
             }
             s++;
         }
+
         if ((char)c == '\0')
         {
             return (char *)s;
         }
+
+        return (char *)last;
     }
 
-    return (char *)last;
+    return NULL;
 }
 
 /*
