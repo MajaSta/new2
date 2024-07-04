ce602ed1ca12d95f81d3811d50d9b375f87c0942
MajaSta <142170733+MajaSta@users.noreply.github.com>
2024-07-04 12:27:34 +0000

    Update libft.h

--- libft.h
+++ libft.h
@@ -6,11 +6,11 @@
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
-int  ft_isalnum(int c)
-int     ft_isalpha(unsigned char c);
+int    ft_isalnum(int c);
+int    ft_isalpha(unsigned char c);
 int		ft_isascii(int i);
 int     ft_isdigit (char c);
-int ft_isprint(int c)
+int   ft_isprint(int c);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
